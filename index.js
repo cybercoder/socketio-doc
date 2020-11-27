fd87dabdd2b620e0ce651988a906d1701ace1907
Asghar <you@asghar.biz>
2020-11-27 14:22:07 +0330

    Add socket.io client

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ get_files(source)
     results=results.flat();
     let emitLinks = createLinks(results.filter(r=>r.action==='listen'));
     let converter = new Converter();
-    console.log(extracted_docs_to_html(results));
+
     writeFileSync('./templates/default/index.html',
     `<!DOCTYPE html>
     <html lang="en">
@@ -37,14 +37,26 @@ get_files(source)
             <title>Socket.io-doc</title>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/picnic">
             <link rel="stylesheet" href="./css/style.css">
+            <script src="https://cdn.socket.io/socket.io-3.0.3.min.js"></script>
         </head>
         <body>
-            <header class="flex one center">
-                <span>Head</span>
-            </header>
+            <div class="flex two">
+                <div class="full half-900">
+                    <span>
+                        1dsfsd
+                    </span>
+                </div>
+                <div class="full half-900">
+                    <div class="three flex">
+                        <input id="server" value="ws://localhost:3000" class="two-third" type="text" placeholder="socket entry point" style="margin:0;">
+                        <button onClick="toggleConnect()" id="connectBtn" class="sixth shyButton" style="margin:0;padding:0">Connect</button>
+                        <button class="sixth pseudo button" style="margin:0;padding:0">Settings</button>
+                    </div>
+                </div>
+            </div>
             <div class="flex three demo">
                 <div class="full sixth-900">
-                    <span>
+                    <span class="emitsMenu">
                         ${converter.makeHtml(emitLinks)}
                     </span>
                 </div>
@@ -59,7 +71,40 @@ get_files(source)
                 
             </footer>
         </body>
-    </html>`
+    </html>
+    <script>
+        var socket;
+        function toggleConnect() {
+            if (socket && socket.connected) {
+                socket.disconnect();
+                let toggleConnectBtn=document.getElementById("connectBtn");
+                toggleConnectBtn.textContent='Connect' ;
+                return toggleConnectBtn.classList.remove('success');
+            }
+
+            socket = io.connect(document.getElementById('server').value, {transports: ['websocket'],});
+            
+            socket.on('connect',()=>{
+                let toggleConnectBtn=document.getElementById("connectBtn");
+                toggleConnectBtn.textContent='Disconnect';
+                toggleConnectBtn.classList.add('success');
+            });
+
+            var onevent = socket.onevent;
+            socket.onevent = function (packet) {
+                var args = packet.data || [];
+                onevent.call (this, packet);    // original call
+                packet.data = ["*"].concat(args);
+                onevent.call(this, packet);      // additional call to catch-all
+            };
+
+            socket.on("*",function(event,data) {
+                console.log(event);
+                console.log(data);
+            });
+        }
+    </script>
+    `
     )
 
     
